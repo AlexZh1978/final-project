317869818fc9cf0cc574b684eabeabeaad4771ba
Алексей <alzhur1978@gmail.com>
2025-09-18 18:57:54 +0300

    change backend 9

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ app.use((err, req, res, _next) => {
   });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`🚀 Сервер запущен на порту ${PORT}`);
   console.log(`📊 API доступен по адресу: http://0.0.0.0:${PORT}/api`);
   console.log('📊 API Version: 2.2.7.8.');
